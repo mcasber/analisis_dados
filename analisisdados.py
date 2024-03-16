240fdc09249c17f5875eb1576d82b7dfd163fe56
mcasber <145811813+mcasber@users.noreply.github.com>
2024-03-16 20:04:59 -0300

    antes de subir a github

--- analisisdados.py
+++ analisisdados.py
@@ -1,6 +1,6 @@
 import random
-import matplotlib.pyplot as plt
 import csv
+import matplotlib.pyplot as plt
 
 def main(numero_de_intentos):
     tiros = []
@@ -60,17 +60,21 @@ def main(numero_de_intentos):
 
 
     probabilidades = [prob_2, prob_3, prob_4, prob_5, prob_6, prob_7, prob_8, prob_9, prob_10, prob_11, prob_12]
+    x=[2,3,4,5,6,7,8,9,10,11,12]#creo las referencia del eje x para el grafico
     
-    # Guardar probabilidades en CSV
+    #Guardar probabilidades en CSV
     guardar_probabilidades_en_csv(probabilidades)
 
     print(f'Probabilidad de obtener un 7 con DOS dados en {numero_de_intentos} tiros es = {prob_7}')
-    print(f'Probabilidad de obtener un 9 con DOS dados en {numero_de_intentos} tiros es = {prob_9}')
-
-    y=[prob_2,prob_3,prob_4,prob_5,prob_6,prob_7,prob_8,prob_9,prob_10,prob_11,prob_12]
-    x=[2,3,4,5,6,7,8,9,10,11,12]
-    plt.plot(x,y, 'bs-')
-    print(plt.show())
+        
+    #GRAFICA
+    plt.style.use('bmh')
+    plt.plot(x,probabilidades,color = 'blue', alpha= 0.5, linewidth= 18,label='dos dados')
+    plt.title(f'Simulacion de tiro con dos dados en {numero_de_intentos} lanzamientos')
+    plt.xlabel('suma de los 2 dados')
+    plt.ylabel('Probabilidad')
+    plt.legend()
+    plt.show()
 
 def guardar_probabilidades_en_csv(probabilidades, nombre_archivo='basedados.csv'):
     with open(nombre_archivo, 'a', newline='') as csvfile:
--- df_analisisdados.py
+++ df_analisisdados.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('C:\\Users\\Mariano\\basedados.csv',sep=",",header=0)
 
 #print(df.info())
-print(df.head(3))         
+#print(df.head())         
 #print(type(df))
 #print(df.iloc[9,10])#acces for index
 #print(df['2'])#accedo por name column
 #df2= df[df['7']>0.17]
 #print(df2)
-#print(df.describe())
+
 
 df['suma'] = df.apply (lambda x : x['2']+x['3']+x['4']+x['5']+x['6']+x['7']+x['8']+x['9']+x['10']+x['11']+x['12'], axis=1)
-print(df.head(3))
+#print(df.head())
+print(df.describe())
+
+sns.relplot(data = df)
+plt.show()
