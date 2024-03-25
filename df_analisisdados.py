35336f628199232fe5670b05e63de76b464d514f
mcasber <145811813+mcasber@users.noreply.github.com>
2024-03-24 22:45:19 -0300

    solo detalles de ordenar codigo

--- df_analisisdados.py
+++ df_analisisdados.py
@@ -13,8 +13,8 @@ df = pd.read_csv(ruta,sep=",",header=0)
 #print(type(df))
 #print(df.iloc[9,10])#acces for index
 #print(df['2'])#accedo por name column
-#df2= df[df['7']>0.17]
-#print(df2)
+df2= df[df['7']>0.17]
+print(df2)
 
 
 #df['suma'] = df.apply (lambda x : x['2']+x['3']+x['4']+x['5']+x['6']+x['7']+x['8']+x['9']+x['10']+x['11']+x['12'], axis=1)
