aee5501547e34bd40fe44f97dd4a53d888be72dc
mcasber <145811813+mcasber@users.noreply.github.com>
2024-06-17 18:06:17 -0300

    actualizo

--- df_analisisdados.py
+++ df_analisisdados.py
@@ -13,13 +13,15 @@ df = pd.read_csv(ruta,sep=",",header=0)
 #print(type(df))
 #print(df.iloc[9,10])#acces for index
 #print(df['2'])#accedo por name column
-df2= df[df['7']>0.17]
-print(df2)
-
+#df2= df[df['7']>0.17]
+#print(df2)
 
 #df['suma'] = df.apply (lambda x : x['2']+x['3']+x['4']+x['5']+x['6']+x['7']+x['8']+x['9']+x['10']+x['11']+x['12'], axis=1)
 #print(df.head())
+
 print(df.describe())
 
 sns.relplot(data = df)
 plt.show()
+
+df.to_csv('dfdados.csv') #guardo el df con las actualizaciones si las hubiere
